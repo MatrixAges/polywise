7f89f2f6c262beb483b37ba04a26524836a8ce99
1yasa <xiewendao@gmail.com>
2026-02-03 15:01:16 +0800

    📝 docs(types): add ContextResult type import

--- packages/polywise/src/Polywise.ts
+++ packages/polywise/src/Polywise.ts
@@ -29,7 +29,8 @@ import type {
 	SingleSearchArgs,
 	ExecuteCotArgs,
 	RecallNodesByKeywordsArgs,
-	StrengthenRelatedEdgesArgs
+	StrengthenRelatedEdgesArgs,
+	ContextResult
 } from './types'
 
 @singleton()
