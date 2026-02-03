4adef412bb2b22fd388fd6aa8a628db61bc60465
1yasa <xiewendao@gmail.com>
2026-02-03 18:00:05 +0800

    ♻️ refactor(consts): rename user schema to user_space for clarity

--- packages/polywise/src/consts/schema.ts
+++ packages/polywise/src/consts/schema.ts
@@ -1,4 +1,4 @@
 export const SCHEMA_META = 'meta'
 export const SCHEMA_BRAIN = 'brain'
 export const SCHEMA_KNOWLEDGE = 'knowledge'
-export const SCHEMA_USER = 'user'
+export const SCHEMA_USER = 'user_space'
