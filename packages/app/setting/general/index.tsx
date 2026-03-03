3b30c37fbf1662847e9ad27c893e1e7d51c1de80
1yasa <xiewendao@gmail.com>
2026-03-03 21:34:20 +0800

    ✨ feat(settings): replace theme and language switches with select components ♻️ refactor(settings): update component export wrapper and add state hooks 💄 style(settings): increase max-width and adjust padding for settings page

--- packages/app/setting/general/index.tsx
+++ packages/app/setting/general/index.tsx
@@ -1,14 +1,28 @@
+import { useMemoizedFn } from 'ahooks'
+import { observer } from 'mobx-react-lite'
+
+import { Field, FieldContent, FieldDescription, FieldGroup, FieldTitle } from '@/__shadcn__/components/ui/field'
 import {
-	Field,
-	FieldContent,
-	FieldDescription,
-	FieldGroup,
-	FieldLabel,
-	FieldTitle
-} from '@/__shadcn__/components/ui/field'
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue
+} from '@/__shadcn__/components/ui/select'
 import { Switch } from '@/__shadcn__/components/ui/switch'
+import { locale_options, themes } from '@/appdata'
+import { useGlobal } from '@/context'
 
 const Index = () => {
+	const global = useGlobal()
+	const l = global.locale
+	const t = global.theme
+
+	const setLang = useMemoizedFn(v => l.setLang(v))
+	const setTheme = useMemoizedFn(v => t.setTheme(v))
+
 	return (
 		<div className='flex w-full'>
 			<FieldGroup className='page_wrap gap-0'>
@@ -20,7 +34,25 @@ const Index = () => {
 							synchronization
 						</FieldDescription>
 					</FieldContent>
-					<Switch id='switch-share' />
+					<Select
+						items={themes.map(item => ({ label: item, value: item }))}
+						value={t.theme_source}
+						onValueChange={setTheme}
+					>
+						<SelectTrigger className='workspace_selector'>
+							<SelectValue />
+						</SelectTrigger>
+						<SelectContent align='start'>
+							<SelectGroup>
+								<SelectLabel>Theme</SelectLabel>
+								{themes.map(item => (
+									<SelectItem value={item} key={item}>
+										{item}
+									</SelectItem>
+								))}
+							</SelectGroup>
+						</SelectContent>
+					</Select>
 				</Field>
 				<Field className='border-dev items-center! border-b py-3' orientation='horizontal'>
 					<FieldContent>
@@ -29,7 +61,21 @@ const Index = () => {
 							Select your preferred language for the application interface and notifications
 						</FieldDescription>
 					</FieldContent>
-					<Switch id='switch-share' />
+					<Select items={locale_options} value={l.lang} onValueChange={setLang}>
+						<SelectTrigger className='workspace_selector'>
+							<SelectValue />
+						</SelectTrigger>
+						<SelectContent align='start'>
+							<SelectGroup>
+								<SelectLabel>Language</SelectLabel>
+								{locale_options.map(item => (
+									<SelectItem value={item.value} key={item.value}>
+										{item.label}
+									</SelectItem>
+								))}
+							</SelectGroup>
+						</SelectContent>
+					</Select>
 				</Field>
 				<Field className='border-dev items-center! border-b py-3' orientation='horizontal'>
 					<FieldContent>
@@ -45,4 +91,4 @@ const Index = () => {
 	)
 }
 
-export default $app.memo(Index)
+export default new $app.Handle(Index).by(observer).by($app.memo).get()
