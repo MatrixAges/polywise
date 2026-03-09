ac9f3f8522b57382180ee7ddbb8f4193724f7ecc
1yasa <xiewendao@gmail.com>
2026-03-09 20:15:41 +0800

    ♻️ refactor(polywise): remove unused pipeline and polywise files

--- packages/polywise/src/pipeline.ts
+++
@@ -1,8 +0,0 @@
-import { join } from 'path'
-import { ModelDownloader } from 'node-llama-cpp'
-import { injectable } from 'tsyringe'
-
-import { app } from './consts'
-
-@injectable()
-export default class Index {}
