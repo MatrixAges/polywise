657bd84442958607a6fa3b4f829975262cb1864a
1yasa <xiewendao@gmail.com>
2026-03-13 21:06:36 +0800

    ✨ feat(db): add full-text search for article urls

--- packages/polywise/src/db/initSql.ts
+++ packages/polywise/src/db/initSql.ts
@@ -3,6 +3,7 @@ import { env } from '@core/env'
 export default () => {
 	const sql = `
       -- FTS 虚表
+      CREATE VIRTUAL TABLE IF NOT EXISTS article_url_fts USING fts5(url, tokenize='unicode61');
       CREATE VIRTUAL TABLE IF NOT EXISTS chunk_fts USING fts5(keywords,tokenize='unicode61');
 
       -- 向量虚表
@@ -11,17 +12,24 @@ export default () => {
       CREATE VIRTUAL TABLE IF NOT EXISTS edge_vec USING vec0(vectors float[1024]);
       CREATE VIRTUAL TABLE IF NOT EXISTS chunk_vec USING vec0(vectors float[1024]);
 
-      -- 触发器：插入同步
+      -- article url fts 触发器
+      CREATE TRIGGER IF NOT EXISTS article_url_after_insert AFTER INSERT ON article BEGIN
+            INSERT INTO article_url_fts(rowid, url) VALUES (new.rowid, new.url);
+      END;
+      CREATE TRIGGER IF NOT EXISTS article_url_after_update AFTER UPDATE ON article BEGIN
+            UPDATE article_url_fts SET url = new.url WHERE rowid = old.rowid;
+      END;
+      CREATE TRIGGER IF NOT EXISTS article_url_after_delete AFTER DELETE ON article BEGIN
+            DELETE FROM article_url_fts WHERE rowid = old.rowid;
+      END;
+
+      -- chunk fts 触发器
       CREATE TRIGGER IF NOT EXISTS chunk_after_insert AFTER INSERT ON chunk BEGIN
             INSERT INTO chunk_fts(rowid, keywords) VALUES (new.rowid, new.keywords);
       END;
-
-      -- 触发器：更新同步
       CREATE TRIGGER IF NOT EXISTS chunk_after_update AFTER UPDATE ON chunk BEGIN
             UPDATE chunk_fts SET keywords = new.keywords WHERE rowid = old.rowid;
       END;
-
-      -- 触发器：删除同步
       CREATE TRIGGER IF NOT EXISTS chunk_after_delete AFTER DELETE ON chunk BEGIN
             DELETE FROM chunk_fts WHERE rowid = old.rowid;
       END;
