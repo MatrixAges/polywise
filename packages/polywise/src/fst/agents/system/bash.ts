d2ee4f89c298018b26fa67a88070f17e4cf21350
1yasa <xiewendao@gmail.com>
2026-04-07 19:26:11 +0800

    ♻️ refactor(bash): remove sandbox environment setup and console logs

--- packages/polywise/src/fst/agents/system/bash.ts
+++ packages/polywise/src/fst/agents/system/bash.ts
@@ -6,14 +6,7 @@ import fs from './fs'
 import type Session from '../../session'
 
 export default async (s: Session) => {
-	const sandboxEnv = { ...process.env } as Record<string, string>
-
-	sandboxEnv.PATH = `/bin:/usr/bin:${sandboxEnv.PATH || ''}`
-
-	console.log('------------')
-	console.log(sandboxEnv)
-
-	const bash = new Bash({ cwd: '/', fs, env: sandboxEnv })
+	const bash = new Bash({ cwd: '/', fs })
 
 	const tools = await getBashTools(s, bash, true)
 
