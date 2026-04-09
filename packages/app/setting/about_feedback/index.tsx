3d756eee11f3e80a184113d18c448937b832fab0
1yasa <xiewendao@gmail.com>
2026-04-09 17:33:30 +0800

    💄 style(about): update layout and styling for about page

--- packages/app/setting/about_feedback/index.tsx
+++ packages/app/setting/about_feedback/index.tsx
@@ -13,76 +13,79 @@ const Index = () => {
 				page_wrap
 			'
 		>
-			<div
-				className='
-					flex
-					items-center justify-center
-					p-2
-					mb-4
-					rounded-xl
-					bg-std-100
-					transition-all
-					hover:fill-std-black
-					fill-std-black
-				'
-				style={{ width: 60, height: 60 }}
-			>
-				<Logo width='100%' height='100%'></Logo>
-			</div>
-			<h1
-				className='
-					mb-1
-					text-std-800 text-3xl font-serif font-bold
-				'
-			>
-				Polywise
-			</h1>
-			<h2 className='text-std-800 font-serif text-lg font-medium'>Where agents live</h2>
-			<div
-				className='
-					flex flex-col
-					items-center
-					gap-2
-					mt-6
-					text-std-600 text-sm font-medium
-					underline
-				'
-			>
-				<div className='flex gap-3'>
-					<a href={about.github} target='_blank'>
-						Github
-					</a>
-					<a href={about.site} target='_blank'>
-						Website
-					</a>
-					<a href={about.docs} target='_blank'>
-						Docs
-					</a>
-					<a href={about.changelog} target='_blank'>
-						Changelog
-					</a>
+			<div className='-mt-6 flex flex-col items-center'>
+				<div
+					className='
+						flex
+						items-center justify-center
+						p-2
+						mb-6
+						rounded-xl
+						border border-border-gray
+						transition-all
+						hover:fill-std-black
+						fill-std-black
+					'
+					style={{ width: 60, height: 60 }}
+				>
+					<Logo width='100%' height='100%'></Logo>
 				</div>
-				<div className='flex gap-3'>
-					<a href={about.issues} target='_blank'>
-						Feedback
-					</a>
-					<a href={about.x} target='_blank'>
-						X(Twitter)
-					</a>
-					<a href={about.email} target='_blank'>
-						Email
-					</a>
-				</div>
+				<h1
+					className='
+						mb-2
+						text-std-800 text-6xl font-bold tracking-wide
+					'
+				>
+					Polywise
+				</h1>
+				<h2 className='text-std-800 text-2xl font-medium tracking-wide'>Where agents live</h2>
 			</div>
-			<span
+			<div
 				className='
 					absolute
 					bottom-6
-					text-std-400 text-sm font-mono
+					flex flex-col
+					items-center
+					text-std-400
 				'
 			>
-				v{version}
-			</span>
+				<div
+					className='
+						flex flex-col
+						items-center
+						gap-1
+						mb-4
+						font-medium
+					'
+				>
+					<div className='flex gap-3'>
+						<a href={about.github} target='_blank'>
+							Github
+						</a>
+						<a href={about.site} target='_blank'>
+							Website
+						</a>
+						<a href={about.docs} target='_blank'>
+							Docs
+						</a>
+						<a href={about.changelog} target='_blank'>
+							Changelog
+						</a>
+					</div>
+					<div className='flex gap-3'>
+						<a href={about.issues} target='_blank'>
+							Feedback
+						</a>
+						<a href={`mailto:${about.email}`} target='_blank'>
+							Email
+						</a>
+						<a href={about.x} target='_blank'>
+							X
+						</a>
+					</div>
+				</div>
+				<span className='font-mono text-sm'>v{version}</span>
+			</div>
 		</div>
 	)
 }
