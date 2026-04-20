d22a2999e747681d5cecb9a8ed7c5747e08c724b
1yasa <xiewendao@gmail.com>
2026-04-20 18:49:24 +0800

    ♻️ refactor(session): add error handling and file creation for missing pin list

--- packages/polywise/src/rpc/session/utils/readPinList.ts
+++ packages/polywise/src/rpc/session/utils/readPinList.ts
@@ -1,10 +1,24 @@
+import path from 'path'
 import { app } from '@core/consts'
 import fs from 'fs-extra'
 
 import normalizePinList from './normalizePinList'
 
 export default async () => {
-	const raw = await fs.readJson(app.pin_path, { throws: false })
+	try {
+		const raw = await fs.readJson(app.pin_path, { throws: false })
 
-	return normalizePinList(raw)
+		return normalizePinList(raw)
+	} catch (error) {
+		const parsed_error = error as NodeJS.ErrnoException
+
+		if (parsed_error.code === 'ENOENT') {
+			await fs.ensureDir(path.dirname(app.pin_path))
+			await fs.writeJson(app.pin_path, [], { spaces: 4 })
+
+			return []
+		}
+
+		return []
+	}
 }
