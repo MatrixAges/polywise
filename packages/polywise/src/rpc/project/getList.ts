56119ce91ce8241c0656bd3837ba3147c8f698d0
1yasa <xiewendao@gmail.com>
2026-04-28 11:18:10 +0800

    ✨ feat(project): add pagination with has_more flag

--- packages/polywise/src/rpc/project/getList.ts
+++ packages/polywise/src/rpc/project/getList.ts
@@ -1,17 +1,22 @@
 import { getProjects, getProjectSessions } from '../../db/services'
 import { p } from '../../utils/trpc'
 
+const page_size = 10
+
 export default p.query(async () => {
 	const projects = await getProjects({ orderBy: 'asc' })
 
 	return Promise.all(
 		projects.map(async project => {
-			const sessions = await getProjectSessions({
+			const session_rows = await getProjectSessions({
 				project_id: project.id,
-				limit: 10
+				limit: page_size + 1
 			})
 
-			return { project, sessions }
+			const has_more = session_rows.length > page_size
+			const sessions = has_more ? session_rows.slice(0, page_size) : session_rows
+
+			return { project, sessions, has_more }
 		})
 	)
 })
