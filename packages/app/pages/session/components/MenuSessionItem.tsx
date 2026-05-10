5ab80855e8f634b558ff863a6860780e145a228c
1yasa <xiewendao@gmail.com>
2026-05-10 14:05:10 +0800

    💄 style(session): refine unread indicator color and title layout

--- packages/app/pages/session/components/MenuSessionItem.tsx
+++ packages/app/pages/session/components/MenuSessionItem.tsx
@@ -17,7 +17,7 @@ const Index = (props: IPropsMenuSessionItem) => {
 
 	const Status = useMemo(() => {
 		if (is_runing) return <Grip className='text-std-400! size-3' />
-		if (unread) return <ArrowLeft className='text-std-300! size-3' />
+		if (unread) return <ArrowLeft className='size-3 text-indigo-500!' />
 		if (pin) return <Pin className='text-std-300! size-3' />
 
 		return null
@@ -44,7 +44,7 @@ const Index = (props: IPropsMenuSessionItem) => {
 			data-session-index={session_index}
 			data-id={item.id}
 		>
-			<div className='min-w-0 flex-1 truncate'>
+			<div className='w-full truncate'>
 				{renaming ? (
 					<RenameInput
 						active={renaming}
