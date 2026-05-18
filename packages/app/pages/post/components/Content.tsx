fb03070a8291b764bd238c27c49377d78773b087
1yasa <xiewendao@gmail.com>
2026-05-18 21:25:28 +0800

    ✨ feat(post): add search toggle and new post creation button

--- packages/app/pages/post/components/Content.tsx
+++ packages/app/pages/post/components/Content.tsx
@@ -1,18 +1,27 @@
-import { Search } from 'lucide-react'
+import { useState } from 'react'
+import { Plus, Search, X } from 'lucide-react'
 import { observer } from 'mobx-react-lite'
+import { useNavigate } from 'react-router'
 
 import { Input } from '@/__shadcn__/components/ui/input'
 import { Tabs } from '@/components'
 
 import { useModel } from '../context'
 import { for_type_tab_items } from '../utils'
-import Header from './Header'
 import List from './List'
 
 import type { PostForType } from '../types'
 
 const Index = () => {
 	const x = useModel()
+	const navigate = useNavigate()
+	const [search_open, setSearchOpen] = useState(() => !!x.current_search)
+	const is_search_open = search_open || !!x.current_search
+
+	const closeSearch = () => {
+		x.setSearch('')
+		setSearchOpen(false)
+	}
 
 	return (
 		<div className='h-full overflow-y-auto'>
@@ -25,7 +34,6 @@ const Index = () => {
 					page_wrap
 				'
 			>
-				<Header></Header>
 				<div className='border-border-light bg-background/70 rounded-[28px] border'>
 					<div
 						className='
@@ -48,25 +56,69 @@ const Index = () => {
 								onClick={value => x.setForType(value as PostForType)}
 							></Tabs>
 						</div>
-						<div className='w-full max-w-[260px]'>
-							<div className='relative'>
-								<Search
-									className='
-										absolute
-										top-1/2
-										left-3
-										size-3.5
-										text-std-300
-										pointer-events-none -translate-y-1/2
-									'
-								></Search>
-								<Input
-									className='h-8 rounded-full pl-8 text-sm'
-									placeholder={`Search ${x.for_type} articles`}
-									value={x.current_search}
-									onChange={event => x.setSearch(event.target.value)}
-								></Input>
-							</div>
+						<div className='flex shrink-0 items-center gap-1.5'>
+							{is_search_open ? (
+								<div className='relative w-[220px]'>
+									<Search
+										className='
+											absolute
+											top-1/2
+											left-3
+											size-3.5
+											text-std-300
+											pointer-events-none -translate-y-1/2
+										'
+									></Search>
+									<Input
+										autoFocus
+										className='
+											h-8
+											pl-8 pr-8
+											rounded-full
+											text-sm
+										'
+										placeholder={`Search ${x.for_type} articles`}
+										value={x.current_search}
+										onChange={event => x.setSearch(event.target.value)}
+									></Input>
+									<button
+										className='
+											absolute
+											top-1 right-1
+											w-6 h-6
+											icon_button small
+										'
+										type='button'
+										title='Close search'
+										onClick={closeSearch}
+									>
+										<X className='size-3.5'></X>
+									</button>
+								</div>
+							) : (
+								<button
+									className='icon_button small'
+									type='button'
+									title='Search posts'
+									onClick={() => setSearchOpen(true)}
+								>
+									<Search className='size-3.5'></Search>
+								</button>
+							)}
+							<button
+								className='icon_button small'
+								type='button'
+								title='New post'
+								onClick={async () => {
+									const id = await x.createPost()
+
+									if (id) {
+										navigate(`/post/${id}`)
+									}
+								}}
+							>
+								<Plus className='size-3.5'></Plus>
+							</button>
 						</div>
 					</div>
 					<div className='flex flex-col gap-2 p-3'>
