7728124e31e00a8980e2edf808356d2efb4903f9
1yasa <xiewendao@gmail.com>
2026-05-25 18:50:19 +0800

    ♻️ refactor(contact): simplify contact page and navigation

--- packages/website/app/[locale]/contact/page.tsx
+++ packages/website/app/[locale]/contact/page.tsx
@@ -51,21 +51,6 @@ const Index = () => {
 						{t('supports.mailto')} &gt;
 					</a>
 				</div>
-				<div
-					className='
-						flex flex-col
-						contact_item lightcard top
-					'
-				>
-					<div className='flex items-center'>
-						<Microphone weight='fill'></Microphone>
-						<h2>{t('supports.influencer.title')}</h2>
-					</div>
-					<span className='desc'>{t('supports.influencer.desc')}</span>
-					<a className='btn_link btn_light' href={mails.member} target='_blank'>
-						{t('supports.mailto')} &gt;
-					</a>
-				</div>
 			</div>
 			<div className='contact_items flex'>
 				<div className='contact_item flex flex-col'>
@@ -88,46 +73,7 @@ const Index = () => {
 						{t_global('visit')} &gt;
 					</Link>
 				</div>
-				<div className='contact_item flex flex-col'>
-					<div className='flex items-center'>
-						<YoutubeLogo weight='fill'></YoutubeLogo>
-						<h2>{t('references.video.title')}</h2>
-					</div>
-					<span className='desc'>{t('references.video.desc')}</span>
-					<a
-						className='btn_link'
-						href={locale === 'en' ? medias.youtube : medias.bilibili}
-						target='_blank'
-					>
-						{t_global('visit')} &gt;
-					</a>
-				</div>
 			</div>
-			{/* <div className='plan_wrap flex flex-col items-center'>
-				<h3 className='plan_title section_title text-center'>{t('plan.title')}</h3>
-				<span className='plan_desc section_desc text-center'>{t('plan.desc')}</span>
-				<div className='plan_items flex flex-col'>
-					{Array.from({ length: Number(t('plan.numbers')) }).map((_, index) => (
-						<div className='plan_item flex' key={index}>
-							<span className='type'>{t(`plan.${index}.type`)}</span>
-							<span className='content'>{t(`plan.${index}.content`)}</span>
-						</div>
-					))}
-				</div>
-			</div> */}
-			{locale === 'zh' && (
-				<div
-					className='
-						box-border
-						flex
-						justify-center
-						w-full
-						wechat
-					'
-				>
-					这里是中文站点的占位联系信息区域，后续会替换为正式的社群、企业合作与支持渠道。
-				</div>
-			)}
 		</div>
 	)
 }
