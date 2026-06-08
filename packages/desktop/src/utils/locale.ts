c582bdad8654c0182a8780b603b4d5ba5daa6829
1yasa <xiewendao@gmail.com>
2026-06-08 12:02:02 +0800

    🐛 fix(locale): map locale codes for i18next

--- packages/desktop/src/utils/locale.ts
+++ packages/desktop/src/utils/locale.ts
@@ -1,9 +1,8 @@
-import { app } from 'electron'
-import i18next from 'i18next'
-
 import en from '@desktop/locales/en'
 import zh_cn from '@desktop/locales/zh-cn'
 import { conf } from '@desktop/utils'
+import { app } from 'electron'
+import i18next from 'i18next'
 
 import type { Lang } from '@app/types'
 
@@ -11,16 +10,23 @@ const conf_lang = await conf.get('lang')
 const sys_lang = app.getLocale().indexOf('zh') !== -1 ? 'zh-cn' : 'en'
 const lang = (conf_lang || sys_lang) as Lang
 
-const locale_map = {
+const i18n_lang_map = {
+	en: 'en',
+	'zh-cn': 'zh-CN'
+} as const
+
+const resource_map = {
 	en: en,
 	'zh-cn': zh_cn
-}
+} as const
+
+const i18n_lang = i18n_lang_map[lang]
 
 i18next.init({
 	initAsync: false,
-	lng: lang,
-	fallbackLng: lang,
+	lng: i18n_lang,
+	fallbackLng: i18n_lang,
 	load: 'currentOnly',
 	returnObjects: true,
-	resources: { [lang]: locale_map[lang] }
+	resources: { [i18n_lang]: resource_map[lang] }
 })
