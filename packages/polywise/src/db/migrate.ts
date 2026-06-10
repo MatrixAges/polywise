5056ade04a09258e77f4d91ea5ee004ee0de980b
1yasa <xiewendao@gmail.com>
2026-06-10 19:28:03 +0800

    🐛 fix(db): use available migrations dir

--- packages/polywise/src/db/migrate.ts
+++ packages/polywise/src/db/migrate.ts
@@ -1,10 +1,23 @@
+import { existsSync } from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { env } from '@core/env'
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
 
-export default () => {
-	const migrations_dir = fileURLToPath(new URL('../drizzle', import.meta.url))
+const getMigrationsDir = () => {
+	const dist_migrations_dir = fileURLToPath(new URL('./drizzle', import.meta.url))
 
-	migrate(env.db, { migrationsFolder: path.resolve(migrations_dir) })
+	if (existsSync(dist_migrations_dir)) {
+		return path.resolve(dist_migrations_dir)
+	}
+
+	const package_migrations_dir = fileURLToPath(new URL('../drizzle', import.meta.url))
+
+	return path.resolve(package_migrations_dir)
+}
+
+export default () => {
+	const migrations_dir = getMigrationsDir()
+
+	migrate(env.db, { migrationsFolder: migrations_dir })
 }
